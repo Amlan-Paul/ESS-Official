2fb4d4d684d0b8d5469d3e0f29c5b0adb073952f
Amlan Paul <87090725+Amlan-Paul@users.noreply.github.com>
2021-10-05 10:41:03 +0600

    Create end.js

---
+++ extrafile/end.js
@@ -0,0 +1,45 @@
+const username = document.querySelector('#username')
+
+const saveScoreBtn = document.querySelector('#saveScoreBtn') 
+
+const finalScore = document.querySelector('#finalScore')
+
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem( 'highScores')) || []
+
+const MAX_HIGH_SCORES = 10
+
+finalScore.innerText = mostRecentScore
+
+username.addEventListener('keyup', () => {
+
+saveScoreBtn.disabled = !username.value
+
+})
+
+saveHighScore = e => {
+
+e.preventDefault()
+
+const score = {
+
+score: mostRecentScore, 
+name: username.value
+
+}
+
+highScores.push(score)
+
+highScores.sort ((a,b) => {
+
+return b.score - a.score
+
+})
+
+highScores.splice(10)
+
+localStorage.setItem( 'highScores', JSON.stringify (highScores)) 
+window.location.assign('/')
+
+}
