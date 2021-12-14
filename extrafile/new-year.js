9fdcd9d98eeb1fa3960fd55d7a38f04da12b7f29
Amlan Paul <87090725+Amlan-Paul@users.noreply.github.com>
2021-12-14 23:42:13 +0600

    Update new-year.js

--- extrafile/new-year.js
+++ extrafile/new-year.js
@@ -4,7 +4,7 @@
     hour = minute * 60,
     day = hour * 24;
 
-  let birthday = "Jan 01, 2021 00:00:00",
+  let birthday = "Jan 01, 2022 00:00:00",
     countDown = new Date(birthday).getTime(),
     x = setInterval(function() {
 
